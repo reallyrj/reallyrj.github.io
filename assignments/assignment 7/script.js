a22d36991aaa43bcd7bc4292f84e0318a77f6346
RJ Lemon <rlemon@email.sc.edu>
2024-02-03 15:34:21 -0500

    first test

--- assignments/assignment 7/script.js
+++ assignments/assignment 7/script.js
@@ -3,8 +3,8 @@ window.onload=()=>{
     const image=document.getElementById("image");
     
     imagebuttom.onclick=()=>{
+        alert("Image changed");
         image.src='images/200x200(2).gif';
-        
     };
 };
 window.onload=()=>{
