1dc7d71d6e15b0e4b8d09fe70a56ea98f6f94c81
RJ Lemon <rlemon@email.sc.edu>
2024-02-04 18:35:34 -0500

    jr

--- assignments/assignment 7/script.js
+++ assignments/assignment 7/script.js
@@ -1,3 +1,23 @@
+
+window.onload = () => {
+    const image=document.getElementById("changeimage").onclick=imagechange;
+
+    const star = document.getElementById("addstars").onclick=starClicked;
+};
+/*image*/
+const imagechange = () => {
+    const image = document.getElementById("clickimage");
+    image.src = "images/200x200(2).gif";
+
+}
+/*star*/
+const starClicked = () => {
+    const stars = document.getElementById("stars");
+    const star=document.createElement("img");
+    star.src="images/star.png";
+    stars.appendChild(star);
+
+}
 window.onload=()=>{
     const imagebuttom=document.getElementById("changeimage");
     const image=document.getElementById("switchimage");
@@ -15,14 +35,3 @@ window.onload = () => {
         rotateImage.style.transform = `rotate(${rotation}deg)`;
     };
 };
-/*star*/
-window.onload = () => {
-    const star = document.getElementById("addstars").onclick=starClicked;
-};
-const starClicked = () => {
-    const stars = document.getElementById("stars");
-    const star=document.createElement("img");
-    star.src="images/stars.png";
-    stars.appendChild(star);
-
-}
