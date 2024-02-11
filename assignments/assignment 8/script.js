b21e9f97af1569965a9155c4766aad280c4b620d
RJ Lemon <rlemon@email.sc.edu>
2024-02-11 15:11:52 -0500

    a8

---
+++ assignments/assignment 8/script.js
@@ -0,0 +1,10 @@
+// Toggles the nav items to show/hide when the hamburger menu is clicked
+const toggleHamburger = () => {
+    document.getElementById("nav-items").classList.toggle("hide");
+  };
+  
+  // Calls the toggleHamburger function when the hamburger menu is clicked
+  window.onload = () => {
+    document.getElementById("hamburger").onclick = toggleHamburger;
+  };
+  
