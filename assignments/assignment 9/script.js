ab2d1ac3ce6e222a7d8a0df51ce58e0f7b7ca33a
RJ Lemon <rlemon@email.sc.edu>
2024-02-18 17:09:51 -0500

    test

---
+++ assignments/assignment 9/script.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    document.getElementById('start').onclick=bounce;
+    document.getElementById('yoga');
+};
+const bounce = (e) => {
+e.preventDefault();
+const a = document.querySelector('a');
+a.innerHTML = 'Stop';
+while(a.innerHTML === 'Stop'){
+ 
+};
+};
+const define = () => {
+    for(let i=0; i<8; i++){
+    }
+};
