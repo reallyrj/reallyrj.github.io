f6dd97aa1562e69a77a6b1d5ed0094b117ebf4d1
RJ Lemon <rlemon@email.sc.edu>
2024-02-29 17:36:29 -0500

    push

--- assignments/assignment 11/script.js
+++ assignments/assignment 11/script.js
@@ -10,14 +10,18 @@ class Tree{
     }
     get section(){
     const section=document.createElement("section");
-    section.classList.add('tree');
-
+    section.classList.add('tree');  
+    
+    
+    
     const h3=document.createElement("h3");
+    h3.classList.add("center");
     h3.innerHTML=this.title;
+    section.append(h3);
 
     const columns = document.createElement("section");
     columns.classList.add("columns");
-    section.append(columns);
+    section.append(columns); 
     
     const imageSection=document.createElement("section");
     columns.append(imageSection);
@@ -68,6 +72,7 @@ class Tree{
 
 }
 
+
 const trees=[];
 trees.push(new Tree("Bald Cypress","Deciduous","Fast","600 Years","wet,swampy soils","Bald cypresses are slow-growing, long-lived trees that regularly reach up to 600 years in age. Bald cypress trees are valued for the rot-resistant heartwood of mature trees, so they have been widely used to make fence posts, doors, flooring, caskets, cabinetry, boats, and more.","cypress.webp"));
 trees.push(new Tree("Eastern Redbud","Deciduous","Fast","60 years","Forest understory","Eastern redbud is a small deciduous tree. Trees typically grow 20 feet in height with a similar spread and have gracefully ascending branches and a rounded shape. Eastern redbud leaves are alternate, simple, broadly heart-shaped and 3 to 5 inches high and wide. Leaves emerge reddish, turning green as they expand.","redbud.webp"));
@@ -75,7 +80,13 @@ trees.push(new Tree("Loblolly Pine","Evergreen","Very Fast","200 years","soils w
 trees.push(new Tree("Southern Magnolia","Evergreen","Slow","80-120 years","Southern magnolia grows best on rich, loamy, moist soils along streams and near swamps in the Coastal Plain","a large, broadleaf evergreen tree that is noted for its attractive glossy dark green leaves and its large, extremely fragrant flowers.","magnolia.jpeg"));
 
 
-
+window.onload=()=>{
 trees.forEach((tree)=>{
     document.getElementById("tree-list").append(tree.section);
 });
+};
+
+document.getElementsByClassName("tree").onclick=()=>{
+    console.log("clicked");
+    document.getElementById("tree-list").append(tree.expandedsection);
+};
